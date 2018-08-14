027abc9f1e5d62557ff45d63bd234b2ef09f47f2
Sarita Raghunath <sarita.raghunath@gmail.com>
2018-08-14 18:24:11 +0000

    automatically backed up by learn

--- app/app.rb
+++ app/app.rb
@@ -6,3 +6,17 @@ class App < Sinatra::Base
   end
 
 end
+
+# rackup app.rb 
+  #starts Sinatra and run code once 
+  #shotgun small ruby gem 
+  #gem install shotgun
+  #rackup and shotgun do the same thing 
+  
+  #routes are part of the app that connect to specific http requests 
+  #route correspondsto a specific page on awebsite that returns html css and ruby
+  #root = '/'
+  # to get to localhost9928/medicines: 
+    get '/medicines' do 
+      wnfk
+    end 
